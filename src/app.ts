f62e82853f5fd9b75fa261c3bc72ab96341ec519
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-17 13:50:19 +0530

    add globalErrorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,31 @@
+import "reflect-metadata";
+
 import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { globalErrorHandler } from "./middlewares/globalErrorHandler";
+
+import authRouter from "./routes/auth";
+import { Config } from "./config";
 
 const app = express();
 
 app.use(
     cors({
+        origin: [Config.ORIGIN_URI!],
         credentials: true,
     }),
 );
+app.use(express.static("public"));
 app.use(cookieParser());
 app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/health", (req: Request, res: Response) => {
     res.send("Hello World!");
 });
+
+app.use("/api/v1/auth", authRouter);
+
+app.use(globalErrorHandler);
 
 export default app;
