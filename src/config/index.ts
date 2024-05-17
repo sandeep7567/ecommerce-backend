ce208d6c7220795c70b1cd70fd08dcbf4239ee6b
sandeep7567 <sandeepthakur7567@gmail.com>
2024-05-17 14:01:40 +0530

    set & get secure file to config

---
+++ src/config/index.ts
@@ -0,0 +1,34 @@
+import { config } from "dotenv";
+import path from "path";
+
+config({
+    path: path.join(__dirname, `../../.env`),
+});
+
+const {
+    PORT,
+    NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    REFRESH_TOKEN_SECRET,
+    JWKS_URI,
+    PRIVATE_KEY,
+    ORIGIN_URI,
+} = process.env;
+
+export const Config = {
+    PORT,
+    NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
+    REFRESH_TOKEN_SECRET,
+    JWKS_URI,
+    PRIVATE_KEY,
+    ORIGIN_URI,
+};
